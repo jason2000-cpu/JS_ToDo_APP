fdcbd4bf50f5f5da3c6522eeddb18462aa2c180f
Edwin Murimi <edwinfmaina19@gmail.com>
2021-08-16 14:38:38 +0300

    The final product

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
 const addNewTodoButton = document.getElementById("add-new-todo");
 const createTodoButton = document.getElementById("create-todo");
+const updateTodoButton = document.getElementById("update-todo");
 const idField = document.getElementById("id-field");
 const timeField = document.getElementById("time-field");
 const bodyField = document.getElementById("body-field");
 const todoList = document.querySelector(".todo-content");
 const div = document.createElement("div");
-const editTodoButton = document.querySelector(".fa-edit");
 
+updateTodoButton.style.display = "none"
 
-
-const Todos = [];
+let Todos = [];
 
 const displayAllTodos = () => {
+	todoList.innerHTML = ""
 
 	if (Todos.length == 0) {
-		div.classList.add("empty-todo");
-		div.innerHTML = `
+
+		todoList.innerHTML += `
+		<div class = "empty-todo">
 		<img src="undraw_empty_xct9.png" alt="empty image" style="width: 50%;">
 		<br>
 		<span style="font-family: 'Fira Sans', sans-serif; font-size: 20px; font-weight: bold;">There are no todos yet...</span>
-		<br>`;
-		todoList.appendChild(div);
-	} else {
-		document.querySelector(".empty-todo").style.display ="none";
+		<br>
+		</div>
+		`;
+	} else {		
 		for(let key in Todos){
 			let todo = Todos[key];
-			// todoList.innerHTML ="";
 			todoList.innerHTML += `
-			<div class="todo-content-item">
+			<div data-id="${todo.id}" class="todo-content-item">
 				<span class="todo-id">▪️ ${todo.id} ▪️</span>
-				<span class="todo-text">${todo.body}</span>
+				${todo.status === "Complete" ? `<span style="text-decoration: line-through;" class="todo-text">${todo.body}</span>` : `<span class="todo-text">${todo.body}</span>`}
 				<span class="todo-date">Created at : ${todo.timestamp}</span>
-				<span class="todo-status complete">▪️ ${todo.status} ▪️</span>
+				${todo.status === "Complete" ? `<span class="todo-status complete">▪️ ${todo.status} ▪️</span>` : `<span class="todo-status incomplete">▪️ ${todo.status} ▪️</span>`}
 				<div style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;" class="actions-window">
 					<i class="far fa-edit"></i>
 					<i class="far fa-trash-alt"></i>
-					<i class="fas fa-check"></i>
+					${todo.status === "Complete" ? "" : '<i class="fas fa-check"></i>'}
 				</div>
 			</div>
 			`;
@@ -46,32 +47,25 @@ const displayAllTodos = () => {
 displayAllTodos();
 
 const addTodo = () => {
-	document.querySelector(".empty-todo").style.display ="none";
 	const id = idField.value;
 	const timestamp = timeField.value;
 	const body = bodyField.value;
 	const status = "Not complete";
 	Todos.push({id,timestamp, body, status})
-	console.log(Todos);
-	todoList.innerHTML += `
-			<div class="todo-content-item">
-				<span class="todo-id">▪️ ${id} ▪️</span>
-				<span class="todo-text">${body}</span>
-				<span class="todo-date">Created at : ${timestamp}</span>
-				<span class="todo-status complete">▪️ ${status} ▪️</span>
-				<div style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;" class="actions-window">
-					<i class="far fa-edit"></i>
-					<i class="far fa-trash-alt"></i>
-					<i class="fas fa-check"></i>
-				</div>
-			</div>
-			`;
 	idField.value = "";
 	timeField.value = "";
+	bodyField.value = "";
+	displayAllTodos();
 }
 
-const editTodo = () => {
-	console.log(Todos);
+const editTodo = (itemId) => {
+	createTodoButton.style.display = "none"
+	updateTodoButton.style.display = "block"
+	const {id, timestamp, status, body} = Todos.filter(todo => todo.id == itemId)[0];
+
+	idField.value = id;
+	timeField.value = getTimeStamp();
+	bodyField.value = body;
 }
 
 const generateID = () => {
@@ -90,7 +84,59 @@ const addNewTodo = () => {
 	timeField.value = getTimeStamp();
 }
 
+const deleteTodo = (itemId) => {
+	Todos = Todos.filter(todo => todo.id != itemId);
+	displayAllTodos();
+}
+
+const updateTodo = () => {
+	const todos = Todos.map(todo=>{
+		if(todo.id === idField.value){
+			todo.status = "Not complete";
+			todo.body = bodyField.value;
+			todo.timestamp = timeField.value;
+			return todo;
+		}else{
+			return todo;
+		}
+	})
+	Todos = todos;
+	idField.value = "";
+	timeField.value = "";
+	bodyField.value = "";
+	displayAllTodos();
+}
+
+const markTodoAsComplete = (itemId) => {
+	const todos = Todos.map(todo=>{
+		if(todo.id === itemId){
+			todo.status = "Complete";
+			return todo;
+		}else{
+			return todo;
+		}
+	})
+	Todos = todos;
+	displayAllTodos();
+}
+
+todoList.addEventListener('click', (e)=>{
+
+	const id = e.target.parentElement.parentElement.dataset.id;
+
+	if(e.target.classList.contains('fa-edit')){
+	  editTodo(id);
+	}
+	
+	if(e.target.classList.contains('fa-trash-alt')) {
+	  deleteTodo(id);
+ 	}
+
+	if(e.target.classList.contains('fa-check')){
+	  markTodoAsComplete(id);
+	}
+})
 
 addNewTodoButton.addEventListener('click', addNewTodo);
 createTodoButton.addEventListener('click', addTodo);
-editTodoButton.addEventListener('click', editTodo);
+updateTodoButton.addEventListener('click', updateTodo);
